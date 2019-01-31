2c5545e580d4a3c0da9b5a884e8b6130a0491960
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-31 03:04:05 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,16 +13,20 @@ end
 
   
 
- def deposit(money)
-   self
+  def deposit(deposit_amount)
+   self.balance += deposit_amount
   end  
 
-def display_balance 
-  "Your balance is $#{@balance}."
-end  
-
-def valid?
-  status == "open" && balance > 0
-end
+  def display_balance 
+    "Your balance is $#{@balance}."
+  end  
+  
+  def valid?
+    status == "open" && balance > 0
+  end
+  
+  def close_account
+    @status = "closed"
+  end 
 
 end
