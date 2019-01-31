7f8b695a402c4f75d18b9d881e0448e8d9a4281f
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-31 03:09:16 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,25 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid?
   end  
-    
   
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+  end  
+  
+  def reverse_transfer 
+    if valid? && sender.balance > amount && self.status == "pending"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "incomplete"
+    else 
+      reject_transfer
+    end 
+  end  
   
 end
